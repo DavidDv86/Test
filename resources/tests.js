1dcf0012e69b46415091a5ead03bdfccdd022256
DavidDv86 <david.dv86@gmail.com>
2013-03-05 11:08:29 +0100

    Initial Version

---
+++ resources/tests.js
@@ -0,0 +1,12 @@
+test("some tests", function () {
+    expect(3);
+
+    ok(true, "passes because true is true");
+    equal("1", 1, "passes because '1' == 1");
+    strictEqual("1", 1, "fails because '1' !== 1");
+});
+
+test("hello test", function () {
+    ok(1 == "1", "Passed!");
+});
+
